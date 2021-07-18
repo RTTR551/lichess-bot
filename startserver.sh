7569a02debeb742c49dee4225ef3bb0780e19f04
Devil-commits <83263569+Devil-commits@users.noreply.github.com>
2021-07-18 12:14:38 +0530

    Update startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -1,4 +1,5 @@
 chmod +x ./engines/stockfish13
 chmod +x ./engines/stockfish13m
-chmod +x ./engines/stockfish14m.exe
+chmod +x ./engines/stockfish14m.
+chmod +x ./engines/stockfish14m
 python3 lichess-bot.py -u
